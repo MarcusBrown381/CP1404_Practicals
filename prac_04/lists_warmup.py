b783a405b2b57c1bee7747d31fca109eae3886b0
MarcusBrown381 <marcus.brown@my.jcu.edu.au>
2019-08-21 15:44:39 +1000

    Attempt Prac 04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,9 @@
+def main():
+    no_of_months = input("How many months? ")
+    count = 0
+    for number in no_of_months:
+        month_income = input("Enter income for month {}:")
+        count += 1
+
+
+main()
