ec22be6e7d39d4d1c198c3d3ea5dd5dc68a530eb
jc605438 <marcus.brown@my.jcu.edu.au>
2019-08-28 15:02:50 +1000

    Complete Walkthrough Example

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,9 +1,21 @@
 def main():
-    no_of_months = input("How many months? ")
-    count = 0
-    for number in no_of_months:
-        month_income = input("Enter income for month {}:")
-        count += 1
+
+    incomes = []
+    no_of_months = int(input("How many months? "))
+
+    for month in range(1, no_of_months + 1):
+        income = float(input("Enter income for month {}:").format(str(month)))
+        incomes.append(income)
+    income_report(incomes, no_of_months)
+
+
+def income_report(incomes, no_of_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, no_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
 main()
