6155a1c11c84cc3b64fd781d24493e29a645a742
jc605438 <marcus.brown@my.jcu.edu.au>
2019-09-11 15:57:39 +1000

    prac_06 attempt guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+class Guitar:
+
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{}, ({}), : , {}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return 2019 - self.year
+
+    def is_vintage(self):
+        if
