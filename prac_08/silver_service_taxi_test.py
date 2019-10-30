4f293cad87c78036212289ffafa19c8ec8f64a30
MarcusBrown381 <marcus.brown@my.jcu.edu.au>
2019-10-30 13:51:56 +1000

    Add Silver Service Taxi

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,11 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    taxi = SilverServiceTaxi("Real-Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+
+
+main()
